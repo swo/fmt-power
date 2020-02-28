5b79d9a42aa4a8a1ee0573fbdcd68e838aa3dd7e
swo <swo@alum.mit.edu>
2020-02-28 14:19:45 -0500

    tweaked

--- analyze-schubert.R
+++ analyze-schubert.R
@@ -1,5 +1,10 @@
 #!/usr/bin/env Rscript --vanilla
 
+# Look at the diarrhea case/control 16S data. Assume "good" donors look like
+# controls and "bad" donors look like cases. Given some N of good and N of bad
+# donors, and some Δp between their efficacies, what's the statistical power of
+# a typical PERMANOVA?
+
 library(tidyverse)
 library(vegan)
 
@@ -48,7 +53,7 @@ subset_dissim <- function(dissim, idx) {
     as.dist()
 }
 
-simulate <- function(n, p0, p1) {
+simulate1 <- function(n, p0, p1) {
   X <- c(rbinom(n, 1, p0), rbinom(n, 1, p1))
 
   idx <- c(sample(q0idx, n), sample(q1idx, n))
@@ -57,24 +62,35 @@ simulate <- function(n, p0, p1) {
   adonis(Y ~ X)
 }
 
-results_fn <- "results.rds"
+simulate <- function(n, p0, p1, n_iter = 1) {
+  control <- list(n = n, p0 = p0, p1 = p1, n_iter = n_iter)
 
-if (file.exists(results_fn)) {
-  results <- readRDS(results_fn)
-} else {
-  results <- crossing(
-    n = c(5, 10, 25, 50),
-    delta_p = c(0.01, 0.25, 1.0),
-    iter = 1:20
-  ) %>%
-    mutate(
-      test = map2(n, delta_p, ~ simulate(.x, 0.5 - .y / 2, 0.5 + .y / 2)),
-      p = map_dbl(test, ~ .$aov.tab$`Pr(>F)`[1])
-    )
+  hash <- digest::digest(control)
+  cache_fn <- str_glue("cache/{hash}.rds")
+  if (file.exists(cache_fn)) return(readRDS(cache_fn)$results)
 
-  saveRDS(results, results_fn)
+  results <- map(1:n_iter, ~ simulate1(n, p0, p1))
+  saveRDS(list(control = control, results = results), cache_fn)
+
+  results
 }
 
+n_iter <- 50
+results <- crossing(
+  n = c(5, 10, 25, 50, 75),
+  delta_p = c(0.01, 0.25, 0.35, 0.50, 1.0),
+) %>%
+  mutate(test = pmap(list(n, delta_p, n_iter), ~ simulate(..1, 0.5 - ..2 / 2, 0.5 + ..2 / 2, ..3))) %>%
+  unnest() %>%
+  mutate(p = map_dbl(test, ~ .$aov.tab$`Pr(>F)`[1]))
+
+# histogram of p's against N and Δp
+plot <- results %>%
+  ggplot(aes(p)) +
+  facet_grid(delta_p ~ n, scales = "free_y") +
+  geom_histogram(position = "dodge", bins = 20)
+
+# plot of powers
 powers <- results %>%
   group_by(n, delta_p) %>%
   summarize(
@@ -88,19 +104,10 @@ powers <- results %>%
     ciu = map_dbl(test, ~ .$conf.int[2])
   )
 
-# plot <- powers %>%
-#   ggplot(aes(n, y = NULL, color = factor(delta_p))) +
-#   geom_crossbar(aes(y = estimate, ymin = cil, ymax = ciu)) +
-#   geom_point(data = results, aes(y = p), position = "jitter")
-
-# plot <- results %>%
-#   ggplot(aes(factor(n), p, color = factor(delta_p))) +
-#   geom_violin() +
-#   geom_point()
-
-plot <- results %>%
-  ggplot(aes(p, fill = factor(n))) +
-  facet_grid(delta_p ~ .) +
-  geom_histogram(position = "dodge", bins = 20)
+plot <- powers %>%
+  ggplot(aes(factor(n), estimate, color = factor(delta_p))) +
+  geom_hline(yintercept = 0.05, linetype = 2) +
+  geom_hline(yintercept = 0.80, linetype = 2) +
+  geom_pointrange(aes(ymin = cil, ymax = ciu), position = position_dodge(width = 0.25))
 
 ggsave("tmp.pdf")
