d177b7deccbec78d0adfe790a691c8ed9aa56c5a
swo <swo@alum.mit.edu>
2020-03-18 14:53:12 -0400

    improved pba

--- pba.R
+++ pba.R
@@ -1,18 +1,12 @@
 #!/usr/bin/env Rscript --vanilla
 
-# Burnashev and Zigangirov
 # https://people.orie.cornell.edu/shane/theses/ThesisRolfWaeber.pdf
-# Plot shows how the posterior pdf shrinks
 
 library(tidyverse)
 
-pc <- 0.75
-max_conf <- 0.99
-n_bin <- 1000
+stochastic_bisection <- function(f, interval, pc = 0.75, n_bin = 1e2, min_conf = 0.99, trace = FALSE) {
+  if (f(interval[2]) <= f(interval[1])) stop("Requires an increasing function")
 
-g <- function(x, n = 1000) rbinom(1, n, 1 - x) / n - 0.8
-
-stochastic_bisection <- function(f, interval, pc, n_bin, min_conf) {
   qc <- 1 - pc
 
   # set up a series of breakpoints at the endpoints and between the bins
@@ -22,8 +16,14 @@ stochastic_bisection <- function(f, interval, pc, n_bin, min_conf) {
 
   # start with a flat prior
   pdf <- rep(1 / n_bin, n_bin)
-
   i <- 1
+
+  if (trace) {
+    trace <- tibble(i = i, x = bin_x, y = pdf)
+  } else {
+    trace <- NULL
+  }
+
   while (max(pdf) < min_conf) {
     # get the median point of the PDF
     cdf <- c(0, cumsum(pdf))
@@ -35,20 +35,19 @@ stochastic_bisection <- function(f, interval, pc, n_bin, min_conf) {
     yn <- f(xn)
 
     if (yn >= 0) {
-      pdf[bin_x >= xn] <- pc * pdf[bin_x >= xn]
-      pdf[bin_x < xn] <-  qc * pdf[bin_x < xn]
-    } else {
       pdf[bin_x >= xn] <- qc * pdf[bin_x >= xn]
       pdf[bin_x < xn] <-  pc * pdf[bin_x < xn]
+    } else {
+      pdf[bin_x >= xn] <- pc * pdf[bin_x >= xn]
+      pdf[bin_x < xn] <-  qc * pdf[bin_x < xn]
     }
 
     # renormalize
     pdf <- pdf / sum(pdf)
 
     i <- i + 1
+    if (trace) trace <- bind_rows(trace, tibble(i = i, x = bin_x, y = pdf))
   }
 
-  list(x = xn, conf = max(pdf), n_iter = i)
+  list(x = xn, conf = max(pdf), n_iter = i, trace = trace)
 }
-
-stochastic_bisection(g, c(0, 1), 0.75, 1e3, 0.99)
