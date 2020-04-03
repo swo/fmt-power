7ccbe8bdde3a03101b257d56a45d0189b00dced1
swo <swo@alum.mit.edu>
2020-04-03 12:26:34 -0400

    moved to sigma_p

--- simulate-sigma.R
+++ simulate-sigma.R
@@ -3,14 +3,29 @@
 source("utils.R")
 
 invlogit <- function(lo) 1 / (1 + exp(-lo))
+logit <- function(p) log(p / (1 - p))
+pdf <- function(p, sigma_lo) dnorm(logit(p), 0, sigma_lo) / (p * (1 - p))
 
-simulate_f <- function(n_donors, patients_per_donor, sigma) {
-  donor_p <- invlogit(rnorm(n_donors, mean = 0, sd = sigma))
+# Given σ_LO, find σ_p
+slo_to_sp <- function(sigma_lo) {
+  f <- function(x) (x - 0.5) ** 2 * pdf(x, sigma_lo)
+  integrate(f, 0, 1)$value
+}
+
+# Reverse
+sp_to_slo <- function(sigma_p, interval = c(1e-6, 10)) {
+  f <- function(x) slo_to_sp(x) - sigma_p
+  uniroot(f, interval)$root
+}
+
+simulate_f <- function(n_donors, patients_per_donor, sigma_p) {
+  sigma_lo <- sp_to_slo(sigma_p)
+  donor_p <- invlogit(rnorm(n_donors, mean = 0, sd = sigma_lo))
   success <- rbinom(n_donors, patients_per_donor, donor_p)
   chisq_p(cbind(success, patients_per_donor - success))
 }
 
-results <- results_f(simulate_f, 1e-4, 3, "cache/sigma")
+results <- results_f(simulate_f, 0, 0.2, "cache/sigma")
 
 results %>%
   select(n_donors, n_patients, effect_size, x, n, estimate, lci, uci) %>%
@@ -18,7 +33,10 @@ results %>%
 
 plot <- plot_f(results) +
   scale_x_continuous(
-    name = expression(paste("Effect size (", sigma[LO], ")"))
-  )
+    name = expression(paste("Effect size (", sigma[p], ")")),
+    breaks = c(0, 0.1, 0.2),
+    labels = c("0", "0.1", "0.2")
+  ) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 ggsave("fig/sigma.pdf")
