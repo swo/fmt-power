eba89d2b774417da6c023ab4eeb039428908a889
swo <swo@alum.mit.edu>
2020-04-09 11:03:34 -0500

    removed many utils

--- simulate-gb.R
+++ simulate-gb.R
@@ -2,6 +2,12 @@
 
 source("utils.R")
 
+# Safe test function
+chisq_p <- function(x) {
+  p <- suppressWarnings(chisq.test(x)$p.value)
+  ifelse(is.na(p), 1.0, p)
+}
+
 simulate_base <- function(n_donors, patients_per_donor, phi, p0, p1) {
   quality <- rbinom(n_donors, 1, phi)
   p <- p0 + quality * (p1 - p0)
@@ -14,8 +20,28 @@ simulate_f <- function(n_donors, patients_per_donor, p1) {
   simulate_base(n_donors, patients_per_donor, 0.5, p0, p1)
 }
 
-results <- results_f(simulate_f, 0.5, 1, "cache/gb")
 
-results %>%
-  select(n_donors, n_patients, effect_size, x, n, estimate) %>%
-  write_tsv("results/gb.tsv")
+simulate_trials <- function(n_donors, patients_per_donor, effect_size) {
+  map_dbl(
+    1:global_n_trials,
+    ~ simulate_f(n_donors, patients_per_donor, effect_size)
+  )
+}
+
+f <- memoise(simulate_trials, cache = cache_filesystem("cache/gb"))
+
+results <- crossing(
+  n_patients = global_n_patients,
+  n_donors = global_n_donors,
+  effect_size = seq(0.5, 1.0, length.out = global_n_grid)
+) %>%
+  mutate(
+    patients_per_donor = n_patients / n_donors,
+    p_values = pmap(list(n_donors, patients_per_donor, effect_size), f),
+    x = map_dbl(p_values, ~ sum(. <= 0.05)),
+    n = map_dbl(p_values, length),
+    estimate = x / n
+  ) %>%
+  select(n_donors, n_patients, effect_size, x, n, estimate)
+
+write_tsv(results, "results/gb.tsv")
--- utils.R
+++ utils.R
@@ -4,40 +4,5 @@ library(memoise)
 set.seed(5)
 global_n_grid <- 11
 global_n_trials <- 1e3
-global_n_donors = c(2, 4, 6, 12),
-
-# Safe test function
-chisq_p <- function(x) {
-  p <- suppressWarnings(chisq.test(x)$p.value)
-  ifelse(is.na(p), 1.0, p)
-}
-
-# Simulation functions ------------------------------------------------
-# simulate_f :: n_donors, patients_per_donor, effect_size -> p_value
-
-results_base <- crossing(
-  n_donors = c(2, 4, 6, 12),
-  n_patients = c(12, 24, 48, 96, 192),
-) %>%
-  filter(n_patients > n_donors) %>%
-  mutate(patients_per_donor = n_patients / n_donors)
-
-results_f <- function(
-  results_base, simulate_f, lower, upper, cache,
-  n_grid = global_n_grid, n_trials = global_n_trials
-) {
-  simulate_trials <- function(n_donors, patients_per_donor, effect_size) {
-    map_dbl(1:n_trials, ~ simulate_f(n_donors, patients_per_donor, effect_size))
-  }
-
-  f <- memoise(simulate_trials, cache = cache_filesystem(cache))
-
-  results_base %>%
-    crossing(effect_size = seq(lower, upper, length.out = n_grid)) %>%
-    mutate(
-      p_values = pmap(list(n_donors, patients_per_donor, effect_size), f),
-      x = map_dbl(p_values, ~ sum(. <= 0.05)),
-      n = map_dbl(p_values, length),
-      estimate = x / n
-    )
-}
+global_n_patients <- c(12, 24, 48, 96, 192)
+global_n_donors <- c(2, 4, 6, 12)
