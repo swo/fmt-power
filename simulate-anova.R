7fa37ee91e1c88ceb9bb0349e9441a9f38ec73c7
swo <swo@alum.mit.edu>
2020-04-09 11:03:54 -0500

    cleaned up anova

--- simulate-anova.R
+++ simulate-anova.R
@@ -5,16 +5,31 @@ source("utils.R")
 # Effect size is σ_D / (σ_D + σ_P). Convert that to σ_D / σ_P as the "between
 # variance" in the ANOVA power function.
 
-results <- results_base %>%
-  crossing(effect_size = seq(0, 1 - 1e-12, length.out = global_n_grid)) %>%
+anova_power <- function(n_donors, patients_per_donor, effect_size) {
+  if (patients_per_donor == 1) {
+    1.0
+  } else {
+    power.anova.test(
+      groups = n_donors, n = patients_per_donor,
+      within.var = 1, between.var = effect_size
+    )$power
+  }
+}
+
+results <- crossing(
+  n_patients = global_n_patients,
+  n_donors = global_n_donors,
+  effect_size = seq(0, 1 - 1e-12, length.out = global_n_grid)
+) %>%
+  filter(n_patients > n_donors) %>%
   mutate(
+    patients_per_donor = n_patients / n_donors,
     between_var = 1 / ((1 / effect_size) - 1),
     estimate = pmap_dbl(
       list(n_donors, patients_per_donor, between_var),
-      ~ power.anova.test(groups = ..1, n = ..2, within.var = 1, between.var = ..3)$power
+      anova_power
     )
-  )
+  ) %>%
+  select(n_donors, n_patients, effect_size, estimate)
 
-results %>%
-  select(n_donors, n_patients, effect_size, estimate, lci, uci) %>%
-  write_tsv("results/anova.tsv")
+write_tsv(results, "results/anova.tsv")
